cd3965b06689104ded0d8b36037eac832fb40cfd
m1dnas <felipesandimguliermone@gmail.com>
2022-07-10 17:29:58 -0700

    updated betty

--- all_the_functions.c
+++ all_the_functions.c
@@ -49,11 +49,11 @@ int form_s(va_list arg)
 	return (0);
 }
 
-/** form_d - prints in %d format.
+/** 
+ * form_d - prints in %d format.
  * @arg: argument to print.
  * Return: an integrer.
- */
-
+ **/
 int form_d(va_list arg)
 {
 	unsigned int abs, aux, num, account;
@@ -83,7 +83,11 @@ int form_d(va_list arg)
 	}
 	return (account);
 }
-
+/**
+ * form_i - prints in %i format
+ * @arg: argument to print
+ * Return: an int
+ **/ 
 int form_i(va_list arg)
 {
 	return (form_d(arg));
