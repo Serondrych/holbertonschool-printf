2cc01cff9e143e05cbe18ca728d6128a37355449
m1dnas <felipesandimguliermone@gmail.com>
2022-07-10 17:18:48 -0700

    added some things

--- main.h
+++ main.h
@@ -5,11 +5,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct form
+{
+	char *form;
+	int (*f)(va_list arg);
+} form_t;
 /**
  * all the created functions
  **/
+int (*func(char c))(va_list arg);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _stringlength(char *str);
+int form_c(va_list arg);
+int form_s(va_list arg);
+int form_d(va_list arg);
+int form_i(va_list arg);
 
 #endif
